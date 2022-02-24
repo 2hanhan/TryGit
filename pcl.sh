15f4871600684c2d826d7d95c96328434cf2fe34
要啥啥不会 <73740375+2hanhan@users.noreply.github.com>
2022-02-24 19:18:14 +0800

    Update pcl.sh

--- pcl.sh
+++ pcl.sh
@@ -26,12 +26,12 @@ sudo apt install libboost-all-dev
 git clone https://github.com/PointCloudLibrary/pcl.git
 cd pcl/
 #设置版本
-git checkout tags/pcl-1.8.1
+git checkout tags/pcl-1.9.1
 #编译安装
 mkdir build
 cd build
 cmake  -DCMAKE_BUILD_TYPE=Release \
--DCMAKE_INSTALL_PREFIX=/usr/local/pcl/pcl-1.8.1 \
+-DCMAKE_INSTALL_PREFIX=/usr/local/pcl/pcl-1.9.1 \
 ..
 make -j14
 sudo make install
