4dc3a37b292de8c9dfa50fb6604b29d207aa1639
2hanhan <1085771291@qq.com>
2022-05-10 09:34:01 +0800

    curl

--- 环境配置/C++LIB/cmake-3-13-2.sh
+++ 环境配置/C++LIB/cmake-3-13-2.sh
@@ -1,7 +1,9 @@
 wget https://github.com/Kitware/CMake/releases/download/v3.13.2/cmake-3.13.2.tar.gz
 tar -zxvf cmake-3.13.2.tar.gz
 cd cmake-3.13.2
-./bootstrap
+#./bootstrap
+# curl的问题因为ros中使用cmake自动下载会失败所以重新设置cmake的编译选项如下
+./bootstrap --system-curl -- -DCMAKE_BUILD_TYPE:STRING=Release
 make -j8 
 sudo make install
 
